ce20ff7e5e1a43c3011c3f9c1114cc62cde850c5
AnnaKlaudija <anna.klaudija.ivina@gmail.com>
2019-11-30 11:57:18 +0200

    Saliku lietas

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class AppLauncher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Pamācība kā izmantot: aprakstīt funkcijas ");
+		Scanner sc = new Scanner(System.in);
+
+		String userInput = sc.nextLine();
+
+
+		String [] splitted = xxxxxxxx.split(" "); // atdalīt funkciju un datumu un input
+		//nulitajā pozīcijā, to do storage
+//tekošo datumu pārveidot today
+
 
 	}
 
