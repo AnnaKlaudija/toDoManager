8658140b339302d63568e9a8609e17d63aef8343
kripsevica <laura.kripsevica@gmail.com>
2019-11-30 22:48:04 +0200

    Input

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -3,13 +3,29 @@ import java.util.Scanner;
 public class AppLauncher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter text in a specific form : action(add, remove, edit) date(DD/MM/YYYY) text");
 
-
-		System.out.println("Pamācība kā izmantot: aprakstīt funkcijas ");
 		Scanner sc = new Scanner(System.in);
-
 		String userInput = sc.nextLine();
+
+		//System.out.println(userInput.length());
+
+		String[] action = userInput.split(" ");
+		System.out.println(action[0]); //Tiek atdalīts pirmais vārds no String
+
+		//System.out.println(action.length);
+
+		String[] date = userInput.split(" ");
+		System.out.println(date[1]); // //Tiek atdalīts otrais vārds no String
+		//System.out.println(date.length);
+
+		int textBefore = ((date.length + action.length)); // Text daļu gribēju atdalīt ar substring f-ju, taču tā atdala zinot konkrēto kārtas Nr.simbolam no kā atdalīt
+//Tā kā šis kārtas 'nr. ne vienmŗ būs tāds pats, tad nepieciešams saskaitīt action + date simbolu skaitu un šeit sākas problēma - nesaprotu kāpēc bet lietojot .length rāda rezultātā cik vārdi ir ievadīti nevis cik simboli??!!
+
+		System.out.println(textBefore);
+
+		String taskText = userInput.substring(textBefore);
+		System.out.println(taskText);
 
 	}
 
