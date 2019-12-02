95beff1a03b383788b58e1f20743ad682b57f3fb
kripsevica <laura.kripsevica@gmail.com>
2019-12-02 22:03:42 +0200

    AppLauncher

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,34 +1,46 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
+import action.TaskStorage;
+
 public class AppLauncher {
 
 	public static void main(String[] args) {
 		System.out.println("Enter text in a specific form : action(add, remove, edit) date(DD/MM/YYYY) :text");
 
+		TaskStorage taskStorage = new TaskStorage();
 		Scanner sc = new Scanner(System.in);
 		String userInput = sc.nextLine();
-		if (userInput != "exit") {
+		//while (userInput != "exit") {
 
-			String[] action = userInput.split(" ");
-			System.out.println(action[0]);
+		String[] action = userInput.split(" ");
+		System.out.println(action[0]);
 
-			//System.out.println(action.length);
-
-
-		} else {
-			System.out.println("done");
-			//break;
-		}
-
-
-
-
-		//System.out.println(userInput.length());
-
+		//System.out.println(action.length);
 
 
 		String[] date = userInput.split(" ");
+		System.out.println(date[1]);
+		//System.out.println(date.length);
+
+		int textBefore = (action[0].length() + date[1].length()+1);
+
+
+		String taskText = userInput.substring(textBefore);
+
+		System.out.println(taskText.trim());
+
+		if(action[0].equals("add") ) {
+			taskStorage.addTask(date[1], taskText);
+		}
+
+	}
+}
+
+
+
+
+	String[] date = userInput.split(" ");
 		System.out.println(date[1]);
 		//System.out.println(date.length);
 
