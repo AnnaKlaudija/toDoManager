dd1502f5e45098a97732debc59b06e4081304436
kripsevica <laura.kripsevica@gmail.com>
2019-12-02 22:04:49 +0200

    Storage

---
+++ src/TaskStorage.java
@@ -0,0 +1,19 @@
+package action;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TaskStorage {
+
+	private Map<String, List<String>> tasks;
+
+	public TaskStorage() {
+		tasks = new HashMap<>();
+	}
+
+	public void addTask(String date, String taskText) {
+
+	}
+
+}
