10f83253b1b9bd37ddcb1db3f741e7cf032b198f
kripsevica <laura.kripsevica@gmail.com>
2019-12-02 20:26:11 +0200

    Input 02.12.2019.

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,32 +1,57 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AppLauncher {
 
 	public static void main(String[] args) {
-		System.out.println("Enter text in a specific form : action(add, remove, edit) date(DD/MM/YYYY) text");
+		System.out.println("Enter text in a specific form : action(add, remove, edit) date(DD/MM/YYYY) :text");
 
 		Scanner sc = new Scanner(System.in);
 		String userInput = sc.nextLine();
+		if (userInput != "exit") {
+
+			String[] action = userInput.split(" ");
+			System.out.println(action[0]);
+
+			//System.out.println(action.length);
+
+
+		} else {
+			System.out.println("done");
+			//break;
+		}
+
+
+
 
 		//System.out.println(userInput.length());
 
-		String[] action = userInput.split(" ");
-		System.out.println(action[0]); //Tiek atdalīts pirmais vārds no String
 
-		//System.out.println(action.length);
 
 		String[] date = userInput.split(" ");
-		System.out.println(date[1]); // //Tiek atdalīts otrais vārds no String
+		System.out.println(date[1]);
 		//System.out.println(date.length);
 
-		int textBefore = ((date.length + action.length)); // Text daļu gribēju atdalīt ar substring f-ju, taču tā atdala zinot konkrēto kārtas Nr.simbolam no kā atdalīt
-//Tā kā šis kārtas 'nr. ne vienmŗ būs tāds pats, tad nepieciešams saskaitīt action + date simbolu skaitu un šeit sākas problēma - nesaprotu kāpēc bet lietojot .length rāda rezultātā cik vārdi ir ievadīti nevis cik simboli??!!
 
-		System.out.println(textBefore);
+		String[] arrOfStr = userInput.split(":");
 
-		String taskText = userInput.substring(textBefore);
-		System.out.println(taskText);
+		for (String a : arrOfStr)
+			System.out.println(a);
 
+		String taskText = userInput.substring(3);
+		// System.out.println(taskText);
+
+		HashMap<String [], String> taskList = new HashMap<>();
+		taskList.put(date, taskText);
+
+
+
+
+		System.out.println("Task List: " + taskList);
 	}
+}
+
+
+
 
 }
